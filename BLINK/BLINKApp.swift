e7dabc7ec4abb5b477fd23122b05afaa854d4631
Shrikrishna <123809369+Krishna7420@users.noreply.github.com>
2026-05-28 14:39:20 +0530

    Initial Commit

---
+++ BLINK/BLINKApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLINKApp.swift
+//  BLINK
+//
+//  Created by Shrikrishna Thodsare on 28/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BLINKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
