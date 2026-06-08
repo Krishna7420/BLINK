c8e002a57ee5459f35ccfa1f774d6bc8db9480d4
Shrikrishna <123809369+Krishna7420@users.noreply.github.com>
2026-06-08 12:41:22 +0530

    Required Changes and Alteration !

---
+++ BLINK/AppState.swift
@@ -0,0 +1,15 @@
+//
+//  AppState 2.swift
+//  BLINK
+//
+//  Created by Shrikrishna Thodsare on 28/05/26.
+//
+
+
+import SwiftUI
+import Combine
+
+class AppState: ObservableObject {
+    
+    @Published var showSplash = true
+}
---
+++ BLINK/Views/SplashView.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  BLINK
+//
+//  Created by Shrikrishna Thodsare on 28/05/26.
+//
+
